844aead2b4f2eddc7118e7848cbbea6bec460c9d
skmu104 <skmu104@aucklanduni.ac.nz>
2019-07-31 18:26:54 +1200

    initial front end

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Header from './components/Header/Header'
+
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+// code for heaader
+
+import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        // template for header, replace with context dependent header
+        <div className="header">
+            <h1 className='headerText'>template</h1>
+        </div>
+    )
+}
